525223f9ae661fecdbda729834d00c82c2cc0e86
muhamadpadila <144421993+muhamadpadila@users.noreply.github.com>
2023-11-20 13:50:37 +0700

    Add files via upload

---
+++ src/components/Global/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle} from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Glass+Antiqua&family=Lexend:wght@200&family=Noto+Sans+Linear+B&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    margin: 0;
+}
+`
+export default GlobalStyle;
